560628b2d79474193cf2f60ea725059faa5f736c
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2025-07-07 05:52:52 +0200

    Fix spec loader tests and update plan

--- Tests/GeneratorTests/SpecLoaderTests.swift
+++ Tests/GeneratorTests/SpecLoaderTests.swift
@@ -3,18 +3,24 @@ import XCTest
 
 final class SpecLoaderTests: XCTestCase {
     func testLoadValidSpec() throws {
-        let url = Bundle.module.url(forResource: "valid", withExtension: "yaml")!
+        let url = Bundle.module.url(forResource: "valid",
+                                    withExtension: "yaml",
+                                    subdirectory: "Fixtures")!
         let spec = try SpecLoader.load(from: url)
         XCTAssertEqual(spec.info.title, "Valid API")
     }
 
     func testLoadInvalidSpecThrows() throws {
-        let url = Bundle.module.url(forResource: "invalid", withExtension: "yaml")!
+        let url = Bundle.module.url(forResource: "invalid",
+                                    withExtension: "yaml",
+                                    subdirectory: "Fixtures")!
         XCTAssertThrowsError(try SpecLoader.load(from: url))
     }
 
     func testLoadPartialSpec() throws {
-        let url = Bundle.module.url(forResource: "partial", withExtension: "yaml")!
+        let url = Bundle.module.url(forResource: "partial",
+                                    withExtension: "yaml",
+                                    subdirectory: "Fixtures")!
         let spec = try SpecLoader.load(from: url)
         XCTAssertNil(spec.paths)
         XCTAssertNil(spec.components)
